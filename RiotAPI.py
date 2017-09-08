938fb2b919f760aca6b54e7f93358c31ff8881dd
Jaime Echeverría <rpggrenade@gmail.com>
2017-09-08 00:56:35 -0300

    5 Seed Json files. Constants. Data finder.

---
+++ RiotAPI.py
@@ -0,0 +1,84 @@
+import RiotConstants as Consts
+import requests
+import json
+import random
+import time
+
+class RiotAPI(object):
+
+    def __init__(self, api_key, region=Consts.REGIONS['KR']):
+        self.api_key = api_key
+        self.region = region
+        self.accountId = 0
+        self.matchId = 0
+
+    def _request(self, api_url, params=[]):
+        args = {'api_key': self.api_key}
+        for key, value in params:
+            if key not in args:
+                args[key] = value
+
+        response = requests.get(
+            Consts.URL['base'].format(
+                region=self.region,
+                request=api_url
+            ),
+            params=args)
+        return response.json()
+
+    def get_random_seed(self, json_name):
+        json_file = open(json_name)
+        json_data = json.load(json_file)
+
+        matches_list = list(json_data['matches']) #just a temp key until I find the actual term
+        match = random.choice(matches_list) #get a random match from the list
+
+        self.update_random_user(match)
+
+    def update_random_user(self, match):
+        users_list = list(match['participantIdentities'])  # temp key
+        user = random.choice(users_list)['player']
+
+        self.accountId = user['accountId']
+        self.region = user['currentPlatformId']
+
+    def get_random_ranked_match(self):
+        ranked_matches = self.get_by_id(self.accountId,type='User Matches')
+        matches = list(ranked_matches['matches'])
+        return random.choice(matches)
+
+    def get_by_id(self, id, type='Match'):
+        api_url = Consts.REQUEST_TYPES[type].format(
+            id=id
+        )
+        return self._request(api_url)
+
+
+    #Plan for getting data:
+    #1. Get a random player from a random match from a random seed data file
+    #2. Get the accountId & region from said user and find the ranked matches they've played (User matches in the constants)
+    #3. Get a random match, store its data after checking for specific data types (possibly finding out tiers)
+    #4. Repeat until a certain amount of matches have been stored
+
+current_key = 'RGAPI-cc071f73-f60a-4d5c-bf52-73bcd2a92211' #Changes every day
+
+def main():
+    api = RiotAPI(current_key)
+    number = random.randint(1,5)
+    #number = 4
+    json_name = "matches" + str(number) + ".json"
+    api.get_random_seed(json_name)
+    while True:
+        try:
+            ranked_match = api.get_random_ranked_match()
+            api.matchId = ranked_match['gameId']
+            match_data = api.get_by_id(api.matchId)
+        except:
+            match_data = api.get_by_id(api.matchId)
+        print(match_data)
+        #save it
+        time.sleep(1)
+        api.update_random_user(match_data)
+
+if __name__ == '__main__':
+    main()
---
+++ RiotConstants.py
@@ -0,0 +1,42 @@
+URL = {
+    'base':'https://{region}.api.riotgames.com/lol/{request}'
+}
+
+QUEUES = {
+    'Solo-Ranked': 4,
+    'Duo-Ranked': 6,
+    'Team-Ranked-5v5': 42,
+    'Team-Ranked-3v3': 41,
+    'Ranked-Draft-5v5': 410,
+    'Ranked-Solo-TeamBuilder': 420,
+}
+
+REQUEST_TYPES = {
+    'Champions':'platform/v3/champions',
+    'Champion':'platform/v3/champions/{id}',
+    'Items':'static-data/v3/items',
+    'Item':'static-data/v3/items/{id}',
+    'Spells':'static-data/v3/summoner-spells',
+    'Spell':'static-data/v3/summoner-spells/{id}',
+    'Status':'status/v3/shard-data',
+    'Masteries':'platform/v3/masteries/by-summoner/{id}',
+    'Match':'match/v3/matches/{id}',
+    'User Matches':'match/v3/matchlists/by-account/{id}',
+    'Runes':'platform/v3/runes/by-summoner/{id}'
+}
+
+REGIONS = {
+    'BR':'BR1', #Brazil
+    'LAN': 'LA1', #Latin America North
+    'LAS': 'LA2', #Latin America South
+    'EUNE':'EUN1', #European North East
+    'EUW':'EUW1', #European West
+    'JP':'JP1', #Japan
+    'KR':'KR', #Korea
+    'NA':'NA', #North America
+    'NA1':'NA1', #Newer North America
+    'OCE':'OC1', #Oceania
+    'TR':'TR1', #Turkey
+    'RU':'RU', #Russia
+    'PBE':'PBE1' #
+}
